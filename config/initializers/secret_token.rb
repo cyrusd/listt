d779f839da9257012e9ef7b1aaa4b0fc7ad78132
Cyrus Durgin <cyrusd@gmail.com>
2012-01-15 23:44:55 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listt::Application.config.secret_token = '10177557e237bb7f684782d0cbefee172a094abbc4e544ca6bea23a8cd9a88d18de121d579459cf35b783c6231521d3399819388476bf76d48d44fbf17bcce89'
