af603c0706450c7652d511142633dbb25c908909
Cyrus Durgin <cyrusd@gmail.com>
2012-01-17 13:20:44 -0800

    added pre-association callback to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,12 @@
 class List < ActiveRecord::Base
   acts_as_audited
-  has_many :list_items
+  has_many :list_items, :before_add => :check_list_item_type
+
+private
+
+  def check_list_item_type (list_item)
+    if ((self.type + "Item") != list_item.type)
+      throw "cannot associate list item type #{list_item.type} with list type #{self.type}"
+    end
+  end
 end
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,3 +1,4 @@
 class ListItem < ActiveRecord::Base
   acts_as_audited
+  belongs_to :list
 end
