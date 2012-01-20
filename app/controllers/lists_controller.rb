badc50948ec3ce2f7235c00b2477efb20370d43a
Cyrus Durgin <cyrusd@gmail.com>
2012-01-20 10:27:00 -0800

    started moving permissions around

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,29 +2,17 @@ class ListsController < ApplicationController
   respond_to :html, :json
 
   def index
-    view = ""
-
     if user_signed_in?
       @lists = current_user.lists
-      view   = "lists/user_index"
     else
       @lists = List.where(:visibility => List::Visibility::PUBLIC)
-      view   = "lists/index"
     end
 
-    render view
+    respond_with @lists
   end
 
   def show
     @list = List.find(params[:id])
-    view  = "lists/show"
-
-    unless @list.visibility == List::Visibility::PUBLIC || (user_signed_in? && current_user.can_view?(@list))
-      flash[:alert] = "Sorry, that list is private."
-      @list = nil
-      view = "lists/permission_denied"
-    end
-
-    render view
+    respond_with @list
   end
 end
